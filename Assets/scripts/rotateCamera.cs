61d82cefc7d9370845ad3bb84d797b369725cc4a
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-11-01 20:32:03 +0100

    use finger instead of mouse coordinates

--- Assets/scripts/rotateCamera.cs
+++ Assets/scripts/rotateCamera.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class rotateCamera : MonoBehaviour {
     float horizontal;
     float vertical;
+    float turnSpeedMouse = 10.0f;
     public Transform container;
-    float turnSpeedMouse = 100.0f;
 
     void Update()
     {
-        //Using mouse
-        horizontal = Input.GetAxis("Mouse X");
-        vertical = Input.GetAxis("Mouse Y");
+        Vector2 finger = Input.GetTouch(0).deltaPosition;
 
-        //This is made in order to avoid rotation on Z, just by typing 0 on Zcoord isn’t enough
+        //This is made in order to avoid rotation on Z, just typing 0 on Zcoord isn’t enough
         //so the container is rotated around Y and the camera around X separately
-        container.Rotate(new Vector3(0, horizontal * (-1), 0f) * Time.deltaTime * turnSpeedMouse);
-        transform.Rotate(new Vector3(vertical, 0, 0) * Time.deltaTime * turnSpeedMouse);
-
+        container.Rotate(new Vector3(0, finger.x * (-1), 0f) * Time.deltaTime * turnSpeedMouse);
+        transform.Rotate(new Vector3(finger.y, 0, 0) * Time.deltaTime * turnSpeedMouse);
     }
 }
