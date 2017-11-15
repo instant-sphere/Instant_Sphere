6329f7395811fbed1b2d839d1091b03cc6f5395a
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-11-16 00:22:03 +0100

    add automatic reset of camera angle with animation

--- Assets/scripts/rotateCamera.cs
+++ Assets/scripts/rotateCamera.cs
@@ -1,46 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/* This class control camera rotations for both automatic and manual mode */
 public class rotateCamera : MonoBehaviour {
     float mTurnSpeedMouse = 10.0f;
     Vector2 mDelta;
-    bool mAutomaticRotation = false;
+    bool mIsAutomaticRotationEnable = false;
+    const float treshold = 2.0f;
 
     public Transform container;
 
     /* Called once per frame */
     void Update()
     {
-        if (mAutomaticRotation)
-            SetDelta();
-        else if(Input.touchCount > 0)
+        if (mIsAutomaticRotationEnable)
+            mDelta = ComputeDelta();
+        else if (Input.touchCount > 0)
             mDelta = Input.GetTouch(0).deltaPosition;
 
         //This is made in order to avoid rotation on Z, just typing 0 on Zcoord isn’t enough
         //so the container is rotated around Y and the camera around X separately
-        container.Rotate(new Vector3(0.0f, - mDelta.x, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
+        //container.Rotate(new Vector3(0.0f, - mDelta.x, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
         transform.Rotate(new Vector3(mDelta.y, 0.0f, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
 
         mDelta = Vector2.zero;
     }
 
-    /* Set delta vector for automatic rotation */
-    private void SetDelta()
+    /* Compute delta vector for automatic rotation */
+    Vector2 ComputeDelta()
     {
-        mDelta.x = 3.0f;
-        mDelta.y = 0.0f;
+        Vector2 d = Vector2.zero;
+        d.x = 2.5f;
+        float actualRotation = transform.rotation.eulerAngles.x;
+        
+        if (actualRotation != 0.0f)
+            if (actualRotation > treshold || (360.0f - actualRotation) > treshold)
+                if (actualRotation > 180.0f)
+                    d.y = treshold;
+                else
+                    d.y = -treshold;
+            else
+               d.y = -actualRotation;
+        return d;
     }
 
     /* Enable automatic rotation */
     public void AutomaticRotation()
     {
-        mAutomaticRotation = true;
+        mIsAutomaticRotationEnable = true;
     }
 
     /* Enable manual rotation */
     public void ManualRotation()
     {
-        mAutomaticRotation = false;
+        mIsAutomaticRotationEnable = false;
     }
 }
