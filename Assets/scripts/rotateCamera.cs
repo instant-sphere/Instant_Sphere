5cf41409dd03e6f460e3e09d81601001fda35ad4
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-11-16 00:33:43 +0100

    uncomment instruction and add support for mouse rotation

--- Assets/scripts/rotateCamera.cs
+++ Assets/scripts/rotateCamera.cs
@@ -16,10 +16,12 @@ public class rotateCamera : MonoBehaviour {
             mDelta = ComputeDelta();
         else if (Input.touchCount > 0)
             mDelta = Input.GetTouch(0).deltaPosition;
+        else if (Input.GetMouseButton(0))
+            mDelta = new Vector2(Input.GetAxis("Mouse X") * 10.0f, Input.GetAxis("Mouse Y") * 10.0f);
 
         //This is made in order to avoid rotation on Z, just typing 0 on Zcoord isn’t enough
         //so the container is rotated around Y and the camera around X separately
-        //container.Rotate(new Vector3(0.0f, - mDelta.x, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
+        container.Rotate(new Vector3(0.0f, - mDelta.x, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
         transform.Rotate(new Vector3(mDelta.y, 0.0f, 0.0f) * Time.deltaTime * mTurnSpeedMouse);
 
         mDelta = Vector2.zero;
