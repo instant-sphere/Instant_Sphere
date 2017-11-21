214bfe13a9cd52c4ab56428d5725f8ad254b3f49
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-11-22 00:28:24 +0100

    sync with AEI repo

---
+++ Assets/scripts/skybox_manager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public sealed class skybox_manager : MonoBehaviour
+{
+    Material mDefaultSkybox;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        mDefaultSkybox = RenderSettings.skybox;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+		
+	}
+
+    public void DefineNewSkybox(byte[] data)
+    {
+        Texture2D t = new Texture2D(2, 2);
+        t.LoadImage(data);
+
+        // TODO : convert equirectangular to cubemap and don't create a new skybox from the shader "Skybox/Equirectangular"
+        /*Cubemap cmap = new Cubemap(t.height, TextureFormat.RGB24, false);
+        cmap.SetPixels(t.GetPixels(), CubemapFace.PositiveX);
+        cmap.filterMode = FilterMode.Trilinear;
+        cmap.Apply();*/
+
+        Material m = new Material(Shader.Find("Skybox/Equirectangular"));
+        m.SetTexture("_Tex", t);
+
+        RenderSettings.skybox = m;
+    }
+
+    public void ResetSkybox()
+    {
+        RenderSettings.skybox = mDefaultSkybox;
+    }
+}
