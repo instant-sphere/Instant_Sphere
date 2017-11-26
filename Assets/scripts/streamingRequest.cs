a84124cf23d8e600e7d49c6213cdd89e061eb37b
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-11-26 02:47:01 +0100

    sync with AEI repo

---
+++ Assets/scripts/streamingRequest.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class streamingRequest
+{
+    HttpWebRequest mWebRequest;
+    Thread mThread;
+    ReaderWriterLockSlim mAccessImage;
+    byte[] mLastFullImage;
+    string mData;
+
+    public streamingRequest(string URL, string jsonData)
+    {
+        mAccessImage = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        mWebRequest = WebRequest.Create(URL) as HttpWebRequest;
+
+        mWebRequest.Method = "POST";
+        mWebRequest.Timeout = (int)(30 * 10000f);
+        mWebRequest.ContentType = "application/json;charset=utf-8";
+
+        byte[] postBytes = Encoding.Default.GetBytes(jsonData);
+        mWebRequest.ContentLength = postBytes.Length;
+
+        Stream reqStream = mWebRequest.GetRequestStream();
+        reqStream.Write(postBytes, 0, postBytes.Length);
+        reqStream.Close();
+
+        mThread = new Thread(Run);
+        mThread.Start();
+    }
+
+    public void Abort()
+    {
+        mThread.Abort();
+        mWebRequest.Abort();
+    }
+
+    public byte[] GetLastReceivedImage()
+    {
+        mAccessImage.EnterWriteLock();
+        byte[] ret = mLastFullImage;
+        mLastFullImage = null;
+        mAccessImage.ExitWriteLock();
+        return ret;
+    }
+
+    void Run()
+    {
+        Stream stream = mWebRequest.GetResponse().GetResponseStream();
+
+        BinaryReader reader = new BinaryReader(new BufferedStream(stream), new ASCIIEncoding());
+
+        while (true)
+        {
+            char c;
+            char[] cs = new char[256];
+            int i = 0;
+            do
+            {
+                c = reader.ReadChar();
+            } while (c != 'h');
+
+            reader.ReadChar();
+
+            while (c != '\n')
+            {
+                c = reader.ReadChar();
+                cs[i++] = c;
+            }
+
+            reader.ReadChar();
+            reader.ReadChar();
+
+            string s = new string(cs);
+            int blockSize = int.Parse(s);
+
+            byte[] data = new byte[blockSize];
+            int readByte = 0;
+            while (readByte != blockSize)
+            {
+                readByte += reader.Read(data, readByte, blockSize - readByte);
+            }
+
+            mAccessImage.EnterWriteLock();
+            mLastFullImage = data;
+            mAccessImage.ExitWriteLock();
+        }
+    }
+}
