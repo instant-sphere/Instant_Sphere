da109707f400b14bc816f1a4e36dd9e5a6da1f67
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2017-12-13 11:46:53 +0100

    timeout

---
+++ Assets/scripts/Timeout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    float mTimeBeforeTrigger;
+    Action mCondition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
