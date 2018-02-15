8cbadefa7421e064c5fb7268bcbca84b0d671855
Emilie <emilie.dao@enseirb-matmeca.fr>
2018-02-15 15:22:41 +0100

    local server

---
+++ server.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env nodejs
+
+const http = require('http');
+var PORT = 2000;
+http.createServer((request, response) => {
+	var body = [];
+	// Collects the data in a array
+	request.on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		// Then concatenates and stringifies it
+		body = Buffer.concat(body).toString();
+
+		console.log(decodeURIComponent(body.replace(/\+/g, " ")));
+		response.end(body);
+	});
+}).listen(PORT);
+
+console.log('Server running');
