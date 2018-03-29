06f7c96cdd788a727816f6d61cb6ba641ca6e9db
Jean-Loup Beaussart <jean-loup.beaussart@bordeaux-inp.fr>
2018-03-30 00:53:14 +0200

    monitoring server transfert log files

--- Assets/scripts/MonitoringServer.cs
+++ Assets/scripts/MonitoringServer.cs
@@ -11,122 +11,121 @@ using Debug = UnityEngine.Debug;
 
 public class MonitoringServer : MonoBehaviour
 {
-	// Local VM conf
-//	private string ip = "127.0.0.1";
-//	private string port = "2222";
+    // Local VM conf
+    //	private string ip = "127.0.0.1";
+    //	private string port = "2222";
 
-	string ip = "92.243.29.132";
-	string port = "334";
+    static string PORT = "334";
 
-	public string GetURL()
-	{
-		return "http://" + ip + ":" + port + "/";
-	}
+    public string GetURL()
+    {
+        return "server.instant-sphere.com:" + PORT;
+    }
 
-	void Start()
-	{
-		StartCoroutine(SendRequest());
-	}
+    void Start()
+    {
+        StartCoroutine(SendRequest());
+    }
 
-	/*
+    /*
 	 * Returns the list of logs files names
 	 */
-	private string[] GetLogsFilesNames()
-	{
-		DirectoryInfo dir = new DirectoryInfo(Application.persistentDataPath + "/");
-		FileInfo[] filesInfos = dir.GetFiles();
-		string[] filesNames = new string[filesInfos.Length];
+    private string[] GetLogsFilesNames()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.persistentDataPath + "/");
+        FileInfo[] filesInfos = dir.GetFiles();
+        List<string> filesNames = new List<string>(filesInfos.Length);
 
-		int i = 0;
-		foreach (var f in filesInfos)
-		{
-			filesNames[i] = f.Name;
-			i++;
-		}
+        foreach (var f in filesInfos)
+        {
+            if (f.Extension == ".log")
+            {
+                filesNames.Add(f.Name);
+            }
+        }
 
-		Debug.Log("directory : " + Application.persistentDataPath + "/");
+        return filesNames.ToArray();
+    }
 
-		return filesNames;
-	}
-
-	/*
+    /*
 	 * Concatenates logs content and returns the result as a JSON array
 	 */
-	private string GetLogs()
-	{
-		string[] filenames = GetLogsFilesNames();
-		string res = "[";
-		int i = 1;
-		Logger logger = Logger.Instance;
+    /*private string GetLogs()
+    {
+        string[] filenames = GetLogsFilesNames();
+        string res = "[";
+        int i = 1;
+        Logger logger = Logger.Instance;
 
-		foreach (var f in filenames)
-		{
-			if (!string.IsNullOrEmpty(logger.GetCurrentFileName()) && f != ".log")
-			{
-				string currentFile = logger.GetCurrentFileName() + ".log";
+        foreach (var f in filenames)
+        {
+            if (!string.IsNullOrEmpty(logger.GetCurrentFileName()) && f != ".log")
+            {
+                string currentFile = logger.GetCurrentFileName() + ".log";
 
-				if (currentFile != f)
-				{
-					res += GetFileContent(f);
-					res += ", ";
-				}
-			}
-			i++;
-		}
+                if (currentFile != f)
+                {
+                    res += GetFileContent(f);
+                    res += ", ";
+                }
+            }
+            i++;
+        }
 
-		// Removes last comma
-		if (res.Length > 1)
-		{
-			res = res.Substring(0, res.Length - 2);
-		}
+        // Removes last comma
+        if (res.Length > 1)
+        {
+            res = res.Substring(0, res.Length - 2);
+        }
 
-		res += " ]";
+        res += " ]";
 
-		return res;
-	}
+        return res;
+    }*/
 
-	/**
-	 * Coroutine sending request every [10 seconds]
+    /**
+	 * Coroutine sending log files every [10 seconds]
 	 */
-	IEnumerator SendRequest()
-	{
-		while (true)
-		{
-			// Creates a form containing the data sent
-			WWWForm form = new WWWForm();
-			string data = "{\"data\":[ "
-				   + "{ \"logs\":" + GetLogs() + " }, "
-				   + "{ \"cameraInfo\": " + CameraData.GetCameraInfo() + "}, "
-				   + "{ \"cameraState\": " + CameraData.GetCameraState() + "} "
-				   + " ]";
+    IEnumerator SendRequest()
+    {
+        Logger logger = Logger.Instance;
 
+        while (true)
+        {
+            string[] filenames = GetLogsFilesNames();
 
-			form.AddField("data", data, Encoding.UTF8);
+            foreach (string f in filenames)
+            {
+                // Creates a form containing the data to send
+                WWWForm form = new WWWForm();
+                form.AddBinaryData("logUploader", Encoding.UTF8.GetBytes(GetFileContent(f)), f, "text/plain");
+                /*form.AddField("cameraInfo", CameraData.GetCameraInfo(), Encoding.UTF8);
+                form.AddField("cameraState", CameraData.GetCameraState(), Encoding.UTF8);*/
+                UnityWebRequest www = UnityWebRequest.Post(GetURL(), form);
 
-			UnityWebRequest www = UnityWebRequest.Post(GetURL(), form);
+                // Sends the request and yields until the send completes
+                yield return www.SendWebRequest();
 
-			// Sends the request and yields until the send completes
-			yield return www.SendWebRequest();
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    File.Delete(Application.persistentDataPath + "/" + f);
+                    Debug.Log("SENDING LOG FILE : " + f);
+                }
+            }
+            // Suspends the coroutine execution for the given amount of seconds using scaled time.
+            yield return new WaitForSeconds(10.0f);
+        }
+    }
 
-			// Suspends the coroutine execution for the given amount of seconds using scaled time.
-			yield return new WaitForSeconds(10.0f);
-
-			if (www.isNetworkError || www.isHttpError)
-			{
-				Debug.Log(www.error);
-			}
-			else
-			{
-				Debug.Log("SENDING REPORT : " + data);
-			}
-		}
-	}
-
-	public static string GetFileContent(string filename)
-	{
-		StreamReader streamReader = new StreamReader(Application.persistentDataPath + "/" + filename);
-		string res = streamReader.ReadToEnd();
-		streamReader.Close();
-		return res;
-	}
+    public static string GetFileContent(string filename)
+    {
+        StreamReader streamReader = new StreamReader(Application.persistentDataPath + "/" + filename);
+        string res = streamReader.ReadToEnd();
+        streamReader.Close();
+        return res;
+    }
 }
--- monitoring_server.js
+++ monitoring_server.js
@@ -1,13 +1,37 @@
 #!/usr/bin/env nodejs
-
 const http = require('http');
 const fs = require('fs');
-//var PORT = 2000; // local conf
-var PORT = 334;
+const multer = require('multer');
+const express = require('express');
+var app = express();
+//var PORT = 2000;
+// local conf
+const PORT = 334;
 const LOGS_DIR = '/var/log/instant-sphere/';
 
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, '/var/log/instant-sphere/');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
 
-http.createServer((request, response) => {
+var upload = multer({ storage : Storage}).single('logUploader');
+
+app.post('/',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file: " + err);
+        }
+        res.end("File is uploaded");
+    });
+});
+app.listen(PORT, function() {
+	console.log("Server running on port " + PORT);
+});
+/*http.createServer((request, response) => {
 	var body = [];
 	// Collects the data in a array
 	request.on('data', (chunk) => {
@@ -17,14 +41,11 @@ http.createServer((request, response) => {
 		body = Buffer.concat(body).toString();
 		var data = decodeURIComponent(body.replace(/\+/g, ""));
 		data = data.substring(5, data.length); // removes "data="
-
 		saveLogs(data);
 		response.end(body);
 	});
 }).listen(PORT);
-
-console.log('Server running');
-
+console.log('Server running');*/
 function saveLogs(data) {
 	var date = new Date();
 	var file = date.toUTCString().replace(/ /g,'_');
@@ -37,15 +58,12 @@ function saveLogs(data) {
 	});
 }
 
-
 function countEventOccurrences(log, eventName) {
 	var count = 0;
 	var entry = log["log_entry"];
 	if (entry) {
-
 		// Used to store non duplicate events
 		var events = [];
-
 		entry.forEach(function(e) {
 			if (e["event"] == eventName && !events.some(k => isEquivalent(k, e))) {
 				events.push(e);
@@ -60,23 +78,19 @@ function isEquivalent(a, b) {
 	// Create arrays of property names
 	var aProps = Object.getOwnPropertyNames(a);
 	var bProps = Object.getOwnPropertyNames(b);
-
 	// If number of properties is different,
 	// objects are not equivalent
 	if (aProps.length != bProps.length) {
 		return false;
 	}
-
 	for (var i = 0; i < aProps.length; i++) {
 		var propName = aProps[i];
-
 		// If values of same property are not equal,
 		// objects are not equivalent
 		if (a[propName] !== b[propName]) {
 			return false;
 		}
 	}
-
 	// If we made it this far, objects
 	// are considered equivalent
 	return true;
